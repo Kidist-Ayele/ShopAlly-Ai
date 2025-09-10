7407e65dcd6ee5559b7e141ae65580b8de6542d9
Fenet-damena <fenetdamena74@gmail.com>
2025-09-09 23:57:06 -1200

    sidebar-to-history-page

--- shopally-web/src/app/(main)/history/page.tsx
+++ shopally-web/src/app/(main)/history/page.tsx
@@ -1,5 +1,18 @@
-import ChatHistoryUI from "../../components/Chat/ChatHistoryUI";
+"use client";
+
+import ChatHistoryUI from "@/app/components/Chat/ChatHistoryUI";
+import Sidebar from "@/app/components/Sidebar";
 
 export default function HistoryPage() {
-  return <ChatHistoryUI />;
+  return (
+    <div className="flex">
+      {/* Sidebar */}
+      <Sidebar activePage="history" />
+
+      {/* Main Content */}
+      <div className="flex-1 ml-16 lg:ml-64">
+        <ChatHistoryUI />
+      </div>
+    </div>
+  );
 }
