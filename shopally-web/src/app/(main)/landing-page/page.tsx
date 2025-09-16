f7af137f283d50c874432956230ef6beb692b932
Fenet-damena <fenetdamena74@gmail.com>
2025-09-16 07:10:02 -1200

    landing-page

---
+++ shopally-web/src/app/(main)/landing-page/page.tsx
@@ -0,0 +1,26 @@
+// app/page.tsx
+import Hero from "../../components/LandingPageComponents/Hero";
+import Sidebar from "@/app/components/Sidebar";
+import HowItWorks from "@/app/components/HowItWorks/HowItWorks";
+import PopularSearches from "@/app/components/LandingPageComponents/PopularSearches";
+import FeaturedDeals from "@/app/components/LandingPageComponents/FeaturedDeals";
+import Footer from "@/app/components/LandingPageComponents/Footer";
+
+export default function Home() {
+  return (
+    <main>
+      {/* Sidebar fixed at top */}
+      <Sidebar activePage="home" />
+
+      {/* Sections with spacing */}
+      <div className="space-y-20">
+        <Hero />
+        <HowItWorks />
+        <PopularSearches />
+        <FeaturedDeals />
+      </div>
+
+      <Footer />
+    </main>
+  );
+}
